2a337104d42115468bde19b0d644725f959481de
Jay Son <fogony@gmail.com>
2016-09-24 19:07:31 +1000

    Implement sync to Google Chrome storage

--- google-chrome-domain-swap/storage.js
+++ google-chrome-domain-swap/storage.js
@@ -1,10 +1,24 @@
 var Storage = {
 	store: function (sets) {
-		localStorage.domain_sets = JSON.stringify(sets);
+		var domain_sets = JSON.stringify(sets);
+		localStorage.domain_sets = domain_sets;
+		console.log("Saved", domain_sets);
+		// Sync to Chrome storage
+		chrome.storage.sync.set({"domain_sets":domain_sets}, function() {		
+			console.log("Sync completed");
+        }); 
 	},
-
 	load: function () {
+		// Sync to Chrome storage
+		chrome.storage.sync.get("domain_sets", function(obj) {
+			var sets = obj["domain_sets"];
+			if( ! sets ) { return; }
+			localStorage.domain_sets = sets;
+			console.log("Sync loaded", sets);
+		});
+		
 		var sets = localStorage.domain_sets;
+		console.log("Local loaded", sets);
 		if( ! sets ) { return; }
 		sets = JSON.parse(sets);
 		for( var i in sets ) {
